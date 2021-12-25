845f8f015e9f0daa84d7004ccdf6c3c7012071da
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2021-12-25 20:27:56 +0100

    feature(singly-linked-list):implement the pop method

--- src/singly-linked-list/index.ts
+++ src/singly-linked-list/index.ts
@@ -25,6 +25,11 @@ export default class SinglyLinkedList {
         this.length--;
     }
 
+    /**
+     * Adds a new node with a value at the end of the list
+     * 
+     * @param { any } value - A value to add at the end of the list
+     */
     push<T>(value: T) {
         const node = new Node<T>(value);
 
@@ -37,17 +42,50 @@ export default class SinglyLinkedList {
 
         this.incrementLength();
     }
+
+    /**
+     * Removes the last node in a linked list and returns the value of the node.
+     */
+    pop() {
+        if (!this.length) return null;
+
+        let currentNode: Node | null = this.head;
+        let previousNode: Node = currentNode;
+
+        while(currentNode.next) { 
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        if (this.length === 1) {
+            this.head = this.tail = null;
+        } else {
+            previousNode.setNext(null);
+            this.tail = previousNode;
+        }
+
+        this.decrementLength();
+        return currentNode.value;
+    }
 }
 
 
 const singlyLinkedList = new SinglyLinkedList();
 
-console.log(singlyLinkedList.length);
+
 singlyLinkedList.push('Hello');
-console.log(singlyLinkedList.length);
-singlyLinkedList.push('World');
-console.log(singlyLinkedList.length);
+singlyLinkedList.push('Super');
+singlyLinkedList.push('Sweet');
+singlyLinkedList.push('Planet');
+singlyLinkedList.push('Earth');
 
-console.log(singlyLinkedList.head);
-console.log(singlyLinkedList.tail);
+console.log(singlyLinkedList);
 
+singlyLinkedList.pop();
+console.log(singlyLinkedList);
+singlyLinkedList.pop();
+console.log(singlyLinkedList);
+singlyLinkedList.pop();
+console.log(singlyLinkedList);
+singlyLinkedList.pop();
+console.log(singlyLinkedList);
